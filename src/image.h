994410eea183251fe293d61f14aa88293a381674
sunsetxh <wyc984872769@hotmail.com>
2018-07-20 16:47:25 +0800

    1

--- src/image.h
+++ src/image.h
@@ -126,7 +126,6 @@ public:
 
     void openFile(const FileName &name, int mode = WRITE_READONLY)
     {
-        std::cout << "name: "<<name<<std::endl;
 
     	// Close any file that was left open in this handler
     	if ( !(fimg ==NULL && fhed == NULL) )
